2719a3d43aa82483411e548ea614b4dd26b632f9
7VREW <129406618+7VREW@users.noreply.github.com>
2023-11-22 15:46:22 +0100

    App upload

---
+++ FloatingButton/FloatingButton/FloatingButton.swift
@@ -0,0 +1,49 @@
+//
+//  FloatingButton.swift
+//  Floating Button
+//
+//  Created by Émilio Williame on 26/10/2023.
+//
+
+import SwiftUI
+
+// Animation du bouton
+struct CustomButtonAnimation: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 1.1 : 1)
+            .animation(.bouncy(duration: 0.2, extraBounce: 0.2), value: configuration.isPressed)
+    }
+}
+
+struct FloatingButton: View {
+    
+    var label: String
+    
+    var body: some View {
+        Button(action: {}, label: {
+            ZStack {
+                
+                // Intérieur du bouton
+               RoundedRectangle(cornerRadius: 30)
+                   .frame(width: 200, height: 60)
+                   .foregroundStyle(.ultraThinMaterial)
+                
+                   // Contour du bouton
+                   .overlay(
+                       RoundedRectangle(cornerRadius: 30)
+                           .stroke(LinearGradient(colors: [Color(.systemOrange), Color(.systemYellow)], startPoint: .leading, endPoint: .trailing), lineWidth: 3)
+                   )
+                
+                //Button's label
+               Text(label)
+           }
+        })
+        .buttonStyle(CustomButtonAnimation())
+         
+    }
+}
+
+#Preview {
+    FloatingButton(label: "Valider")
+}
---
+++ FloatingButton/FloatingButton/FloatingButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingButtonApp.swift
+//  FloatingButton
+//
+//  Created by Émilio Williame on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FloatingButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FloatingButton(label: "Valider")
+        }
+    }
+}
